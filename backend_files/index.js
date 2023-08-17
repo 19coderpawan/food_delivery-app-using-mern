ca982d4e587abbe679442e4f04108ecb154ddea6
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-08-18 02:05:52 +0530

    Update index.js
    in this updated file I have connected fronted with the backend server

--- backend_files/index.js
+++ backend_files/index.js
@@ -14,6 +14,21 @@ const connectdb=require('./database');
 const dotenv=require('dotenv').config();
 const port = process.env.Port;
 connectdb();
+const Router=require('./Routes/CreateUser');
+const Router2=require('./Routes/UserLogin');
+
+/* Now once you have created the signup page in the frontend and you have setup everything now you have to allow 
+the crossorign*/
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+})
+
+
 
 /*This line defines a route for the root URL ("/") using the HTTP GET method. 
 When a client makes a GET request to the root URL, the callback function specified here will be executed. 
@@ -23,6 +38,10 @@ app.get('/', (req, res) => {
     to the client that made the request. The res.send() function is used to send content to the client. */
     res.send(`<h1>Hello pawan</h1>`);
 })
+/* here i am going to use the middle ware to creates routes  */
+app.use(express.json())
+app.use('/api',Router)
+app.use('/api',Router2)
 
 
 /*This line starts the Express server and makes it listen on the specified port. 
