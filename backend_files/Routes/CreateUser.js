673e4b751a1cd9f447437b78e81948b41496056d
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-08-18 02:13:18 +0530

    Update CreateUser.js
    In this update i have set the validation on the data that the user will enter to sign up

--- backend_files/Routes/CreateUser.js
+++ backend_files/Routes/CreateUser.js
@@ -4,7 +4,23 @@ const router=express.Router();
 // we are going to create the model on the schema basis so lets import the schema here.
 const UserSchema=require('../models/schema');
 // here we are using post method beacuse we have to post the data in the database.
-router.post('/createuser',async (req,res)=>{
+
+/* Now i also want to set some validation on the data the user is going to send as an req for that 
+we are going to use the express-validation library. in this we have multiple function to validate the data. */
+
+/* firstly import the express-validator*/
+const {body,validationResult}=require('express-validator');
+
+router.post('/createuser',[
+ body('email','invalid email check your email agian!').isEmail(),
+ body('password','invalid password').isLength({min:5}),
+ body('name','length of the name is to short!').isLength({min:3})
+]
+,async (req,res)=>{
+    const error=validationResult(req);
+    if(!error.isEmpty()){
+        return res.status(400).json({error:error.array()});
+    }
     try {
          await UserSchema.create({
             name:req.body.name,
