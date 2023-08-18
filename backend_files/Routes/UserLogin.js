c0f4b2f728f9382a36a64f83dfafd689f5271ed6
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-08-19 02:19:27 +0530

    Update UserLogin.js
    in the useLogin file I have created the json web token to perform authorization and authentication.

--- backend_files/Routes/UserLogin.js
+++ backend_files/Routes/UserLogin.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const Router2=express.Router();
 const UserSchema=require('../models/schema');
+const bcrypt=require('bcrypt');
+const jwt =require('jsonwebtoken');
 const {body,validationResult}=require('express-validator');
 
 Router2.post('/userlogin',[
@@ -17,10 +19,22 @@ Router2.post('/userlogin',[
       if(!userEmail){
         return res.status(400).json({error:"your email doesnt match check it again!"});
       }
-      if(req.body.password !== userEmail.password){
+      // this will compare both the password and return true or false depending upon the scenario.
+      const bcryptedPassword=await bcrypt.compare(req.body.password,userEmail.password)
+      if(!bcryptedPassword){
         return res.status(400).json({error:"your password is incorrect check it again!"});
       }
-      return res.json({success:true});
+      // now once you have checked the password the server will send two things now one is success:true and 
+      // another is a jwt .
+      const data={// this part is the payload part.
+         user:{
+            id:userEmail.id
+         }
+      }
+      // then finally generate the autorization toke.
+      const authtoken=jwt.sign(data,process.env.secrectkey);// this method has payload and secrectkey as argument 
+      // and as per the header let it be default header of the sign method.
+      return res.json({success:true,authtoken:authtoken});
    } catch (error) {
     throw error;
    }
