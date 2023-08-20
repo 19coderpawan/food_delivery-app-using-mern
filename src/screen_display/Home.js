c04ee20aaea593371925a20aee82f5243aa73152
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-08-21 02:10:44 +0530

    Update Home.js
    In this update  i have connected my Home page with the server using the fetch() and store the data in the States I have created using the Statehook and I have also used the useEffect hooks

--- src/screen_display/Home.js
+++ src/screen_display/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import Cards from '../components/Cards'
 import Footer from '../components/Footer'
@@ -6,12 +6,37 @@ import Carousel_comp from '../components/Carousel_comp'
 
 
 const Home = () => {
+  const [food_data_item, setfood_data_item] = useState([]);
+  const [food_category_data, setfood_category_data] = useState([]);
+  const loadData = async () => {
+    let response = await fetch("http://localhost:9001/api/DisplayData", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const jsonresponse = await response.json();
+    console.log(jsonresponse[0], jsonresponse[1]);
+    setfood_data_item([...jsonresponse[0]]);
+    setfood_category_data([...jsonresponse[1]]);
+  }
+  useEffect(() => {
+    loadData();
+  }, []);
   return (
     <>
-      <Navbar></Navbar>
-      <Carousel_comp></Carousel_comp>
-      <Cards></Cards>
-      <Footer></Footer>
+      <div>
+        <Navbar></Navbar>
+        </div>
+      <div>
+        <Carousel_comp></Carousel_comp>
+      </div>
+      <div className='Container'>
+        <Cards></Cards>
+      </div>
+      <div>
+        <Footer></Footer>
+      </div>
     </>
   )
 }
