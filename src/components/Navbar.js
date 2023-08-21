fb041deb6082c5ae425a8b9240f067fe94052d4f
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-08-22 02:43:35 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     <>
       <nav className="navbar navbar-expand-sm ">
         <div className="container-fluid">
-          <Link className="navbar-brand " to="/">
+          <Link className="navbar-brand "  to="/">
             Happy Food
           </Link>
           <button
@@ -23,28 +23,22 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
+                <Link className="nav-link " aria-current="page"  to="/">
                   Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/login">
+                <Link className="nav-link"  to="/login">
                   Login
                 </Link>
               </li>
-             
+              <li className="nav-item">
+                <Link className="nav-link"  to="/Signup">
+                  SignUp
+                </Link>
+              </li>
             </ul>
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
+            
           </div>
         </div>
       </nav>
