a1a35007d1f0d7dce51ced9d0afac84d5f39326b
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-08-23 02:00:24 +0530

    Update Navbar.js
    In this update i have added the logic for the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigation=useNavigate();
+  const changeOccur=()=>{
+    localStorage.removeItem('authtoke');
+    navigation('/login');
+  }
+
+
   return (
     <>
       <nav className="navbar navbar-expand-sm ">
@@ -20,26 +27,49 @@ const Navbar = () => {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <div className="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
               <li className="nav-item">
-                <Link className="nav-link " aria-current="page"  to="/">
+                <Link className="nav-link active fs-5 " aria-current="page"  to="/">
                   Home
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link"  to="/login">
-                  Login
+              {
+                localStorage.getItem('authtoke')
+                ?
+                <li className="nav-item">
+                <Link className="nav-link  fs-5 text-success b-2" aria-current="page"  to="/">
+                  myOrders
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link"  to="/Signup">
-                  SignUp
-                </Link>
-              </li>
+              :""
+              }
             </ul>
-            
+            {
+              !localStorage.getItem('authtoke')
+              ?<div className='d-flex p-2'><Link className="btn bg-white text-success mx-1"  to="/login">
+              Login
+            </Link>
+         
+            <Link className="btn bg-white text-success  mx-1"  to="/Signup">
+              SignUp
+            </Link>
+            </div>
+            :<div className='d-flex ' >
+              <Link className="btn bg-white text-success mx-1"  to="/" onClick={changeOccur}>
+            Logout
+              </Link>
+       
+             <Link className="btn bg-white text-success mx-1"  to="/">
+            MyCart
+             </Link>
           </div>
+            }
+          </div>
+          
+           
+                
+              
         </div>
       </nav>
     </>
