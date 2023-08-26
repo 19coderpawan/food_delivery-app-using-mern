58ed98e66853b6bb14c81332b09d9f3044bb31f5
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-08-26 23:42:57 +0530

    Update App.js
    In this update i have wrpped the router in the contextProvider to make it globally accessible.

--- src/App.js
+++ src/App.js
@@ -9,19 +9,26 @@ import "./App.css"
   Now lets use the React router dom to navigate between the pages or components. for that fist install the 
   package npm i react-router-dom. and then import the components of the react-router-dom package. 
 */
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./screen_display/Signup";
+import { CartProvider } from "./components/ContextReducer";
 
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          < Route exact path="/" element={<Home />} />
-          < Route exact path="/login" element={<Login/>}/>
-        </Routes>
-      </div>
-    </Router>
+    // here i have wraped the routers inside the cartprovider context so that it can be asseces throughout the code
+    // by or in any component.
+    <CartProvider>
+      <Router>
+        <div>
+          <Routes>
+            < Route exact path="/" element={<Home />} />
+            < Route exact path="/login" element={<Login />} />
+            <Route exact path="/Signup" element={<Signup />} />
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
 
   );
 }
